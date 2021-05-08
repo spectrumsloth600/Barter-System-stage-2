b8b0a9d16153a8f383d72119eab6906844eda7eb
spectrumsloth600 <61101947+spectrumsloth600@users.noreply.github.com>
2021-05-08 14:42:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignUpLoginScreen from './screens/SignUpLoginScreen';
+
+export default function App() {
+  return (
+    <SignUpLoginScreen/>
+  );
+}
